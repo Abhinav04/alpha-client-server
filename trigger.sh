16aa760207200ec80fd55af287bbe4752a55ab67
Abhinav Rajendrakumar Bagrecha <abagrech@amazon.com>
2022-07-14 22:48:31 +0530

    Updating trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -2,5 +2,5 @@
 
 #intofiywait - watch for file modification
 while inotifywait -e modify /var/log/secure; do
-  journalctl _SYSTEMD_UNIT=sshd.service | egrep -o "sshd\[[0-9]*\]: (Failure|Accepted|Invalid)" | wc -l > inotres.txt
+  /send_data.sh
 done
