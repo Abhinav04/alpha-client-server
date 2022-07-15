d6984df52b5312f80df3c11effa312a99e881d5e
Abhinav Rajendrakumar Bagrecha <abagrech@amazon.com>
2022-07-15 13:09:38 +0530

    logging per client node

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route('/',methods=['POST'])
 def display_ssh_attempts():
     data = request.json
+    f = open("alpha_clients/" + data["node_hostname"] + ".txt", "w")
+    f.write(data['ssh_attempts'] + " ssh log-in attempts were made at " + data["node_hostname"])
+    f.close()
+    print(data)
     return jsonify(data)
 # main driver function
 if __name__ == '__main__':
