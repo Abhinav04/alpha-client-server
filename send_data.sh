ff2c6c8570fcded35583592613cdce38f4a2fd1d
Abhinav Rajendrakumar Bagrecha <abagrech@amazon.com>
2022-07-15 17:57:57 +0530

    Updating send_data script

--- send_data.sh
+++ send_data.sh
@@ -3,4 +3,4 @@ attempts=$(journalctl _SYSTEMD_UNIT=sshd.service | egrep -o "sshd\[[0-9]*\]: (Fa
 node_hostname=$(hostname)
 echo $attempts
 echo $node_hostname
-curl -i -H "Content-Type: application/json" -X POST -d "{\"node_hostname\":\"$node_hostname\", \"ssh_attempts\":\"$attempts\"}" http://172.31.95.42:5000
+curl -i -H "Content-Type: application/json" -X POST -d "{\"node_hostname\":\"$node_hostname\", \"ssh_attempts\":\"$attempts\"}" http://172.31.95.42:5000/report-data
